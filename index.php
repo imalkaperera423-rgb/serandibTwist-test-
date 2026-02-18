8b96acc82afa4cd656191b2b3df45e19af20bad4
Damith Kodithuwakku <amk1012409@student.hamk.fi>
2026-02-19 00:02:02 +0200

    Refactor index.php to improve structure and styling

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include 'db.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Home - Serandib Twist</title>
     <style>
-        /* --- Smooth Animations & UI --- */
+        
         body { background-color: #fdfaf5; font-family: 'Poppins', sans-serif; }
 
         .category-card {
@@ -55,7 +55,7 @@ include 'db.php';
             transform: scale(1.05);
         }
 
-        /* --- Navbar Dropdown --- */
+        
         .navbar-nav .dropdown-menu {
             display: block !important;
             opacity: 0;
@@ -240,4 +240,5 @@ include 'db.php';
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="script.js"></script>
 </body>
+
 </html>
